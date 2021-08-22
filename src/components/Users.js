8c4d36d1e5328d7af55ba88f87f33f24eae4b660
jasonemarkson <jasonmarkson76@gmail.com>
2021-08-22 14:35:18 -0400

    Done.

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-// add any needed imports here
+// add any needed imports here --> import connect so Users component can access the store
 import { connect } from 'react-redux';
 class Users extends Component {
 
   render() {
     console.log(this.props.users)
+    // create a users variable to map across the users array received from the props passed down from the state -- iterate over each and make into a line element
     let users = this.props.users.map((user, index) => <li key={index}>{user.username}</li>)
     return (
       <div>
+          {/* In addition, display the total number of users curently in the store */}
         {this.props.numberOfUsers}
         <ul>
           {/* Write code here that displays the usernames of all users in the Redux store */}
           {users}
-          {/* In addition, display the total number of users curently in the store */}
+          {/* render users on the DOM */}
         </ul>
       </div>
     )
@@ -27,5 +29,5 @@ const mapStateToProps = (state) => {
   }
 }
 
-// connect this component to Redux
+// connect this component to Redux --> added connect to export default so that our application knows to map the most up to date state to our props
 export default connect(mapStateToProps) (Users)
